a9f7774cd13cb8063ba52cb858e14f52370653d8
noorabrar362 <noorabrar4u@gmail.com>
2025-06-13 10:22:38 +0600

    Hacker.py

---
+++ Hacker.py
@@ -0,0 +1,12 @@
+# zeta.py
+
+# ANSI escape code for red text
+RED = '\033[91m'
+RESET = '\033[0m'
+
+# Angry emoji
+angry_emoji = "😠"
+
+# Print output
+print(f"{angry_emoji} ./)")
+print(f"{RED}You are banned from this experienced group Theme{RESET}")
