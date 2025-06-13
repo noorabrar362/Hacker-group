91a54278a40d1fcd6cf62a6cbaaec0528055b2c2
noorabrar362 <noorabrar4u@gmail.com>
2025-06-13 10:46:24 +0600

    Update Hacker.py

--- Hacker.py
+++ Hacker.py
@@ -1,5 +1,7 @@
 # zeta.py
 
+import time
+
 # ANSI escape code for red text
 RED = '\033[91m'
 RESET = '\033[0m'
@@ -7,6 +9,7 @@ RESET = '\033[0m'
 # Angry emoji
 angry_emoji = "😠"
 
-# Print output
-print(f"{angry_emoji} ./)")
-print(f"{RED}You are banned from this experienced group Theme{RESET}")
+while True:
+    print(f"{angry_emoji} ./)")
+    print(f"{RED}You are banned from this experienced group Theme{RESET}")
+    time.sleep(1)
